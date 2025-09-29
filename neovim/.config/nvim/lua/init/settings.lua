53f0d37f4c3352e2c6820d75bf2a1a1b89fb54cb
Nathan Wynn <nwwynn@gmail.com>
2025-09-29 11:29:28 -0400

    add stylua toml, highlight selection on yank

--- neovim/.config/nvim/lua/init/settings.lua
+++ neovim/.config/nvim/lua/init/settings.lua
@@ -118,3 +118,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})

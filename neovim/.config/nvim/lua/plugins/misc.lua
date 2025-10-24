1a36d5021e5881877737aac7062947927b4fc3c8
Nathan Wynn <nwwynn@gmail.com>
2025-10-24 09:05:50 -0400

    Show hidden files by default

--- neovim/.config/nvim/lua/plugins/misc.lua
+++ neovim/.config/nvim/lua/plugins/misc.lua
@@ -66,7 +66,11 @@ return {
         enabled = true,
         timeout = 3000,
       },
-      picker = { enabled = true },
+      picker = {
+        enabled = true,
+        hidden = true,
+        ignored = true,
+      },
       quickfile = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = true },
